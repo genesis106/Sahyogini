ef89bf08a0e080695c21e10026ae795f4bf9b3aa
Payal Sahu <payalsahu@Payals-MacBook-Air.local>
2025-04-01 16:17:03 +0530

    Fixed logo Issue

--- sahyogini/src/components/Navbar.jsx
+++ sahyogini/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import { Logo } from "./Logo";
+import { Logo } from "./logo";
 import { Button } from "./ui/Button";
 import { useLanguage } from "../context/LanguageContext";
 
