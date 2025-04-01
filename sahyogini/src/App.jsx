2cc71dbd956fb8619965ecd2e2d25751d57a7b13
Payal Sahu <payalsahu@Payals-MacBook-Air.local>
2025-04-01 16:03:44 +0530

    Fixed Footer Issue

--- sahyogini/src/App.jsx
+++ sahyogini/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/footer"; // Import Footer
+import { Footer } from "./components/Footer"; // Import Footer
 import Home from "./pages/Home/home";
 import Signup from "./pages/Auth/signup";
 import Login from "./pages/Auth/login";
